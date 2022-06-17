4a2948d4ff3c6ecf70900c43b0f3b575dc6bc5b3
Julius-Java <emmanueljulius9102016@gmail.com>
2022-06-17 05:38:33 -0700

    command_line_intperpreter.c file modified

--- command_line_interpreter.c
+++ command_line_interpreter.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/*
+ * main function
+ *
+ *
+ *Return: void
+ */
+int main(void)
+{
+	char *buffer;
+	size_t buff_size;
+	int read;
+
+	buffer = malloc(sizeof(size_t) * buff_size);
+
+	while (1)
+	{
+		print("cisfun$: ");
+		read = getline(&buffer, &buff_size, stdin);
+
+		if (read != EOF)
+		{
+			printf("%s\n, buffer);
+		}
+	}
+	return (0);
+}
